5e30d75085c6b8daa2e9db1675724a6e85f3d384
staniecs <145446078+staniecs@users.noreply.github.com>
2023-10-24 12:29:21 +0200

    changed API from OpenWeather to SheCodes

--- src/index.js
+++ src/index.js
@@ -24,22 +24,22 @@ formatDate(now);
 //show weather in searched city and current location
 function showCurrentConditions(response) {
   let h1 = document.querySelector("h1");
-  h1.innerHTML = `${response.data.name} Weather`;
+  h1.innerHTML = `${response.data.city} Weather`;
 
   document.querySelector(
     "#temp"
   ).innerHTML = `${Math.round(
-    response.data.main.temp
+    response.data.temperature.current
   )}°`;
 
   document.querySelector(
     "#condition"
-  ).innerHTML = `${response.data.weather[0].main}`;
+  ).innerHTML = `${response.data.condition.description}`;
 
   document.querySelector(
     "#humidity"
   ).innerHTML = `Humidity: ${Math.round(
-    response.data.main.humidity
+    response.data.temperature.humidity
   )}%`;
 
   document.querySelector(
@@ -51,8 +51,9 @@ function showCurrentConditions(response) {
 
 //search for a city
 function searchCity(city) {
-  let apiKey = "c5f0e59acac64258bb92ed027d20c68f";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "b3b36of7f40tfb2fc5ea76728725e80c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
   axios
     .get(`${apiUrl}`)
     .then(showCurrentConditions);
@@ -73,8 +74,8 @@ form.addEventListener("submit", handleSubmit);
 function findLocation(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let apiKey = "c5f0e59acac64258bb92ed027d20c68f";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  let apiKey = "b3b36of7f40tfb2fc5ea76728725e80c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showCurrentConditions);
 }
 function showLocationWeather(event) {
